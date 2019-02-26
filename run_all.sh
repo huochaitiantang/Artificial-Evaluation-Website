66f1e4b6df9205cb6bfb5269d8bbee5594ccb72a
liuliang <1432249204@qq.com>
2019-02-26 20:10:49 +0800

    adjust

--- run_all.sh
+++ run_all.sh
@@ -4,12 +4,12 @@ DATA_ROOT=static/data
 ITEM_CNTS=(3 4 3 4 2 2 2 2)
 DISPLAY_SIZE=128
 RUN_IP=219.224.168.78
-RUN_PORT_START=8008
+RUN_PORT_START=5000
 NAVIGATOR_CNT=8
 
 DATA_DIRS=("hand_pick_celeba_close" "hand_pick_celeba_smile" "hand_pick_download_close" "hand_pick_download_smile" "hand_pick_eval_gt_our" "hand_pick_eval_gt_cyclegan" "hand_pick_eval_gt_residual" "hand_pick_eval_gt_icgan")
-BETTER_RULES[0]="下列的图像变换为：将人脸的眼睛由闭合转换为睁开。请从选项中选择转换更为成功的、看起来更为逼真的、与原图像色彩差异较小的结果。"
-BETTER_RULES[1]="下列的图像变换为：将人脸由不笑转换为笑。请从选项中选择转换更为成功的、看起来更为逼真的、与原图像色彩差异较小的结果。"
+BETTER_RULES[0]="下列的图像变换为：将人脸的眼睛由闭合转换为睁开。<br>请从选项中选择转换更为成功的、看起来更为逼真的、与原图像色彩差异较小的结果。"
+BETTER_RULES[1]="下列的图像变换为：将人脸由不笑转换为笑。<br>请从选项中选择转换更为成功的、看起来更为逼真的、与原图像色彩差异较小的结果。"
 BETTER_RULES[2]=${BETTER_RULES[0]}
 BETTER_RULES[3]=${BETTER_RULES[1]}
 BETTER_RULES[4]="请从选项中选择看起来更为逼真的、带有笑容的结果。"
